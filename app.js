e5235f730957bb3590bc34854375e9a2f18a6088
HEMANT_CODE007 <hp926591@gmail.com>
2024-10-26 02:05:49 +0530

    The Picture of dustbin and pencile

--- app.js
+++ app.js
@@ -14,7 +14,21 @@ const updateTasList = () =>{
       const tasklist = document.getElementById('task-list')
       tasklist.innerHTML = ''
       
-    }
+      tasks.forEach(task => {
+        const listItem = `
+        <div class="taskItem">
+          <div class="task">
+            <input typr = "checkbox" class="checkbox" />
+            <p>Finish this project</p>
+            </div>
+            <div class="icons">
+            <img src="./img/edit.png" />
+            <img src="./img/bin.png" />
+            </div>
+          </div>
+        `;
+      });
+    };
 
 document.getElementById('newTask').addEventListener('click',function(e){
   e.preventDefault();
